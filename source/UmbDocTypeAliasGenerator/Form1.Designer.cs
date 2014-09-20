3ff55e4e23af9d432ef47ec299ccf6e6c84b0f35
citikiwi <kenwu.sh@gmail.com>
2014-09-20 09:17:49 +0800

    initialize

---
+++ source/UmbDocTypeAliasGenerator/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace UmbDocTypeAliasGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsitebuilderAssembly = new System.Windows.Forms.TextBox();
+            this.btnGenUsitebuilder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "usitebuilder assembly:";
+            // 
+            // txtUsitebuilderAssembly
+            // 
+            this.txtUsitebuilderAssembly.Location = new System.Drawing.Point(129, 41);
+            this.txtUsitebuilderAssembly.Name = "txtUsitebuilderAssembly";
+            this.txtUsitebuilderAssembly.Size = new System.Drawing.Size(355, 20);
+            this.txtUsitebuilderAssembly.TabIndex = 1;
+            this.txtUsitebuilderAssembly.Click += new System.EventHandler(this.txtUsitebuilderAssembly_Click);
+            // 
+            // btnGenUsitebuilder
+            // 
+            this.btnGenUsitebuilder.Location = new System.Drawing.Point(496, 38);
+            this.btnGenUsitebuilder.Name = "btnGenUsitebuilder";
+            this.btnGenUsitebuilder.Size = new System.Drawing.Size(75, 23);
+            this.btnGenUsitebuilder.TabIndex = 2;
+            this.btnGenUsitebuilder.Text = "Generate";
+            this.btnGenUsitebuilder.UseVisualStyleBackColor = true;
+            this.btnGenUsitebuilder.Click += new System.EventHandler(this.btnGenUsitebuilder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 298);
+            this.Controls.Add(this.btnGenUsitebuilder);
+            this.Controls.Add(this.txtUsitebuilderAssembly);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Umbraco DocTypeAlias Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsitebuilderAssembly;
+        private System.Windows.Forms.Button btnGenUsitebuilder;
+    }
+}
+
---
+++ source/UmbDocTypeAliasGenerator/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace UmbDocTypeAliasGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsitebuilderAssembly_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtUsitebuilderAssembly.Text = d.FileName;
+            }
+        }
+
+        StringBuilder _CoderBuilder = new StringBuilder();
+        List<string> _BuildFields = new List<string>();
+        private void btnGenUsitebuilder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (AssemblyFileIsValid(txtUsitebuilderAssembly.Text.Trim()))
+                {
+                    _CoderBuilder.Clear();
+                    _CoderBuilder.Length = 0;
+                    _BuildFields.Clear();
+
+                    var ass = Assembly.LoadFile(txtUsitebuilderAssembly.Text);
+                    foreach (var t in ass.GetTypes().Where(t => Attribute.IsDefined(t, typeof(Vega.USiteBuilder.DocumentTypeAttribute))))
+                    {
+                        GenerateSingleDocType(t, ref _CoderBuilder, ref _BuildFields);
+                    }
+
+                    if (_CoderBuilder.Length > 0)
+                    {
+                        MessageBox.Show(
+                            "Congratulations! Code generated in the clipboard now!"
+                             , "Success"
+                             , MessageBoxButtons.OK);
+
+                        Clipboard.SetText(_CoderBuilder.ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show(
+                            "No any code generated, please confirm you pick the right file."
+                             , "Failure"
+                             , MessageBoxButtons.OK);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message
+                     , "Failure"
+                     , MessageBoxButtons.OK);
+            }
+        }
+
+        private bool AssemblyFileIsValid(string filePath)
+        {
+            return true;
+        }
+
+        private void GenerateSingleDocType(Type docType, ref StringBuilder codeBuilder, ref List<string> builtFields)
+        {
+            codeBuilder.AppendLine();
+            codeBuilder.AppendLine(String.Format("#region {0}", docType.Name));
+            codeBuilder.AppendLine(String.Format("public const string {0} = \"{0}\";", docType.Name));
+            codeBuilder.AppendLine();
+
+            var baseDocType = docType.BaseType;
+            var basePreperties = baseDocType.GetProperties().Select(p => p.Name).ToList();
+            foreach (var p in docType.GetProperties().Where(p => !basePreperties.Contains(p.Name)))
+            {
+                var pname = p.Name;
+                if (!builtFields.Contains(pname))
+                {
+                    builtFields.Add(pname);
+                    codeBuilder.AppendLine(String.Format("public const string {0} = \"{1}\";", pname, pname[0].ToString().ToLower() + pname.Substring(1, pname.Length - 1)));
+                }
+            }
+
+            codeBuilder.AppendLine("#endregion");
+            codeBuilder.AppendLine();
+        }
+    }
+}
